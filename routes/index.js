e317c666ea74fa536a3e1148e759124cae9813fc
Timothy <timothyroy2011@gmail.com>
2021-07-03 08:51:32 +0600

    add button added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
+const Story = require("../models/Story");
 
 //    Login/Landing Page
 //    Get
@@ -12,11 +13,17 @@ router.get("/", ensureGuest, (req, res) => {
 
 //    Dashboard
 //    Get/ dashboard
-router.get("/dashboard", ensureAuth, (req, res) => {
-  console.log(req.user);
-  res.render("dashboard", {
-    name: req.user.firstName,
-  });
+router.get("/dashboard", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ user: req.user.id }).lean();
+    res.render("dashboard", {
+      name: req.user.firstName,
+      stories,
+    });
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
+  }
 });
 
 //    Routes
